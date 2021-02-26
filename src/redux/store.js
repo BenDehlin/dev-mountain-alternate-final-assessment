6f891bcef3ffd523b414c10aacc8e77875f4e59c
BenDehlin <871078+BenDehlin@users.noreply.github.com>
2021-02-25 22:57:59 -0700

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import postReducer from "./postReducer"
 // Above we import our postReducer and pass it in to the combineReducers
 // function below.
 // WE NEED TO DO THE SAME WITH OUR
-// authReducer so that it will put the redux state for our user 
+// userReducer so that it will put the redux state for our user 
 // into the redux store so that we can
 // access that information in any of our components
 
